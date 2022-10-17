f2d733e4f3fb97bcbed3eb3ccc44dc9f7d3d4313
daidaivn <66916003+daidaivn@users.noreply.github.com>
2022-10-17 15:11:13 +0700

    updaet

--- Tek4TV_BTN/Tek4TV_BTN/API/Home.cs
+++ Tek4TV_BTN/Tek4TV_BTN/API/Home.cs
@@ -2,7 +2,7 @@
 
 namespace Tek4TV_BTN.API
 {
-    public class Home
+    public class HomeApi
     {
         public async Task<dynamic> GetPlaylist(string id)
         {
@@ -16,6 +16,142 @@ namespace Tek4TV_BTN.API
                     httpClient.BaseAddress = new Uri(_domain);
                     httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                     string url = $"api/Playlist/json/" + id;
+                    var responseMessage = await httpClient.GetAsync(url);
+                    if (responseMessage.IsSuccessStatusCode)
+                    {
+                        var rs = await responseMessage.Content.ReadAsAsync<List<dynamic>>();
+                        emData = rs;
+                        return emData;
+                    }
+                    else
+                    {
+                        return emData;
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                return emData;
+            }
+
+
+        }
+
+        public async Task<dynamic> GetObject(string id)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var _domain = config["Domain:url"];
+            var emData = new Object();
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri(_domain);
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    string url = $"api/Playlist/json/" + id;
+                    var responseMessage = await httpClient.GetAsync(url);
+                    if (responseMessage.IsSuccessStatusCode)
+                    {
+                        var rs = await responseMessage.Content.ReadAsAsync<dynamic>();
+                        emData = rs;
+                        return emData;
+                    }
+                    else
+                    {
+                        return emData;
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                return emData;
+            }
+
+
+        }
+
+        public async Task<dynamic> GetPlaylistDev(string id)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var _domain = config["Domain:devurl"];
+            List<dynamic> emData = new List<dynamic>();
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri(_domain);
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    string url = $"api/Playlist/json/" + id;
+                    var responseMessage = await httpClient.GetAsync(url);
+                    if (responseMessage.IsSuccessStatusCode)
+                    {
+                        var rs = await responseMessage.Content.ReadAsAsync<List<dynamic>>();
+                        emData = rs;
+                        return emData;
+                    }
+                    else
+                    {
+                        return emData;
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                return emData;
+            }
+
+
+        }
+
+        public async Task<dynamic> GetObjectDev(string id)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var _domain = config["Domain:devurl"];
+            var emData = new Object();
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri(_domain);
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    string url = $"api/Playlist/json/" + id;
+                    var responseMessage = await httpClient.GetAsync(url);
+                    if (responseMessage.IsSuccessStatusCode)
+                    {
+                        var rs = await responseMessage.Content.ReadAsAsync<dynamic>();
+                        emData = rs;
+                        return emData;
+                    }
+                    else
+                    {
+                        return emData;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return emData;
+            }
+
+
+        }
+
+        public async Task<dynamic> GetVideo(int type, int page, int size)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var _domain = config["Domain:url"];
+            List<dynamic> emData = new List<dynamic>();
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri(_domain);
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    string url = $"api/Video/content/" + type + "/" + page + "/" + size;
                     var responseMessage = await httpClient.GetAsync(url);
                     if (responseMessage.IsSuccessStatusCode)
                     {
--- Tek4TV_BTN/Tek4TV_BTN/Controllers/HomeController.cs
+++ Tek4TV_BTN/Tek4TV_BTN/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Tek4TV_BTN.API;
 using Tek4TV_BTN.Models;
 
 namespace Tek4TV_BTN.Controllers
@@ -8,25 +9,24 @@ namespace Tek4TV_BTN.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        HomeApi api = new HomeApi();
+        public HomeController()
         {
-            _logger = logger;
+            //  _menu = menu;
         }
-
         public IActionResult Index()
         {
+            ViewBag.Menus = api.GetPlaylist("menu_f61fb9fe-d512-44f9-a30a-111c31e71f86").Result;
+            ViewBag.Weather = api.GetObject("weather").Result;
+            ViewBag.Trend = api.GetObjectDev("fe4814ab-dce0-4d74-84e8-3a198f08dee9").Result;
+            ViewBag.Headline = api.GetPlaylist("hightlight").Result;
+            ViewBag.Live = api.GetObject("aa13ae69-3ae9-46e3-ab6a-17b6aef3cd66").Result;
+            ViewBag.Home = api.GetPlaylist("group_home_1").Result;
+            ViewBag.Popcast = api.GetObjectDev("d3514cc0-1514-4a57-a4ce-c436da1db2fd").Result;
+            ViewBag.Radio = api.GetObjectDev("77f07e83-1dd2-4a20-b05e-c9e1ae7675f7").Result;
+            ViewBag.Look = api.GetObject("24d6f593-f44e-451b-8901-0f250febc495").Result;
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
