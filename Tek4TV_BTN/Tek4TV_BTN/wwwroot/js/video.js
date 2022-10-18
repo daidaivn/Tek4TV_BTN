473ea96b482f9f57f7dbaca01b68ad6ec891ac87
daidaivn <66916003+daidaivn@users.noreply.github.com>
2022-10-18 08:59:12 +0700

    asss

---
+++ Tek4TV_BTN/Tek4TV_BTN/wwwroot/js/video.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  // scroll-top
+const scrollBtnn = document.querySelector(".btn-scroll-top");
+const btnnVisibility = () => {
+if (window.scrollY > 400) {
+    scrollBtnn.style.visibility = "visible";
+} else {
+    scrollBtnn.style.visibility = "hidden";
+}
+};
+document.addEventListener("scroll", () => {
+btnnVisibility();
+});
+scrollBtnn.addEventListener("click", (e) => {e.preventDefault();
+  window.scroll(0,0);
+  });
